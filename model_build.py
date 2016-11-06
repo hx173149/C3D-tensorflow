a2b04613c57355481b80ce30f356011849d0bae4
hx173149 <hx173149@gmail.com>
2016-11-06 21:00:13 +0800

    update model finetuning from sports1M

--- model_build.py
+++ model_build.py
@@ -1,63 +1,89 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 """Builds the MNIST network.
 
 Implements the inference/loss/training pattern for model building.
 
-1. inference_c3d() - Builds the c3d model as far as is required for running the network
+1. inference() - Builds the model as far as is required for running the network
 forward to make predictions.
+2. loss() - Adds to the inference model the layers required to generate loss.
+3. training() - Adds to the loss model the Ops required to generate and
+apply gradients.
 
 This file is used by the various "fully_connected_*.py" files and not meant to
 be run.
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
+import math
+import re
 import numpy as np
 
 import tensorflow as tf
 
-# The UCF101 dataset has 101 classes, representing the digits 0 through 100.
+import random 
+
+# The MNIST dataset has 10 classes, representing the digits 0 through 9.
 NUM_CLASSES = 101 
 
 # The MNIST images are always 28x28 pixels.
 IMAGE_SIZE = 112 
+#IMAGE_SIZE = 28 
 CHANNELS = 3
+#CHANNELS = 1 
 FRAMES = 16 
+IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE * CHANNELS * FRAMES
+#IMAGE_PIXELS = 28*28*1 
+
 
 "-----------------------------------------------------------------------------------------------------------------------"
 
 
 def conv3d(name, l_input, w, b):
-    #return tf.nn.relu(tf.nn.bias_add(tf.nn.conv3d(l_input, w, strides=[1, 1, 1, 1, 1], padding='SAME'),b), name=name)
-    return tf.nn.bias_add(tf.nn.conv3d(l_input, w, strides=[1, 1, 1, 1, 1], padding='SAME'),b)
+  return tf.nn.bias_add(tf.nn.conv3d(l_input, w, strides=[1, 1, 1, 1, 1], padding='SAME'),b)
+def conv3d_valid(name, l_input, w, b):
+  return tf.nn.relu(tf.nn.bias_add(tf.nn.conv3d(l_input, w, strides=[1, 1, 1, 1, 1], padding='VALID'),b), name=name)
 
-def max_pool3d(name, l_input, k):
-    return tf.nn.max_pool3d(l_input, ksize=[1, k, 2, 2, 1], strides=[1, k, 2, 2, 1], padding='SAME', name=name)
+def max_pool(name, l_input, k):
+  return tf.nn.max_pool3d(l_input, ksize=[1, k, 2, 2, 1], strides=[1, k, 2, 2, 1], padding='SAME', name=name)
 
 def inference_c3d(_X,_dropout,batch_size,_weights,_biases):
   # Convolution Layer
   conv1 = conv3d('conv1', _X, _weights['wc1'], _biases['bc1'])
+
   conv1 = tf.nn.relu(conv1,'relu1')
-  pool1 = max_pool3d('pool1', conv1, k=1)
+  pool1 = max_pool('pool1', conv1, k=1)
 
   # Convolution Layer
   conv2 = conv3d('conv2', pool1, _weights['wc2'], _biases['bc2'])
   conv2 = tf.nn.relu(conv2,'relu2')
-  pool2 = max_pool3d('pool2', conv2, k=2)
+  pool2 = max_pool('pool2', conv2, k=2)
 
   # Convolution Layer
   conv3 = conv3d('conv3a', pool2, _weights['wc3a'], _biases['bc3a'])
   conv3 = tf.nn.relu(conv3,'relu3a')
   conv3 = conv3d('conv3b', conv3, _weights['wc3b'], _biases['bc3b'])
   conv3 = tf.nn.relu(conv3,'relu3b')
-  pool3 = max_pool3d('pool3', conv3, k=2)
+  pool3 = max_pool('pool3', conv3, k=2)
 
   # Convolution Layer
   conv4 = conv3d('conv4a', pool3, _weights['wc4a'], _biases['bc4a'])
   conv4 = tf.nn.relu(conv4,'relu4a')
   conv4 = conv3d('conv4b', conv4, _weights['wc4b'], _biases['bc4b'])
   conv4 = tf.nn.relu(conv4,'relu4b')
-  pool4 = max_pool3d('pool4', conv4, k=2)
+  pool4 = max_pool('pool4', conv4, k=2)
 
   # Convolution Layer
   conv5 = conv3d('conv5a', pool4, _weights['wc5a'], _biases['bc5a'])
@@ -65,9 +91,10 @@ def inference_c3d(_X,_dropout,batch_size,_weights,_biases):
   conv5 = conv3d('conv5b', conv5, _weights['wc5b'], _biases['bc5b'])
   conv5 = tf.nn.relu(conv5,'relu5b')
 
-  pool5 = max_pool3d('pool5', conv5, k=2)
+  pool5 = max_pool('pool5', conv5, k=2)
   
   # Fully connected layer
+  pool5 = tf.transpose(pool5,perm=[0,1,4,2,3])
   dense1 = tf.reshape(pool5, [batch_size, _weights['wd1'].get_shape().as_list()[0]]) # Reshape conv3 output to fit dense layer input
   dense1 = tf.matmul(dense1, _weights['wd1']) + _biases['bd1'] 
   
@@ -78,4 +105,4 @@ def inference_c3d(_X,_dropout,batch_size,_weights,_biases):
   dense2 = tf.nn.dropout(dense2, _dropout)
   # Output, class prediction
   out = tf.matmul(dense2, _weights['out']) + _biases['out']
-  return out 
+  return out
--- train_c3d_ucf101.py
+++ train_c3d_ucf101.py
@@ -1,12 +1,24 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 """Trains and Evaluates the MNIST network using a feed dictionary."""
 # pylint: disable=missing-docstring
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os.path
 import time
 
+import numpy
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
@@ -19,21 +31,25 @@ import numpy as np
 # Basic model parameters as external flags.
 flags = tf.app.flags
 gpu_num = 4 
-flags.DEFINE_float('learning_rate', 1e-3, 'Initial learning rate.')
+#flags.DEFINE_float('learning_rate', 0.0, 'Initial learning rate.')
 flags.DEFINE_integer('max_steps', 20000, 'Number of steps to run trainer.')
-flags.DEFINE_integer('batch_size', 16 , 'Batch size.  '
+flags.DEFINE_integer('batch_size', 20 , 'Batch size.  '
                      'Must divide evenly into the dataset sizes.')
 
 FLAGS = flags.FLAGS
 
-MOVING_AVERAGE_DECAY = 0.999
+MOVING_AVERAGE_DECAY = 0.9999
+
 
 def placeholder_inputs(batch_size):
   """Generate placeholder variables to represent the input tensors.
+
   These placeholders are used as inputs by the rest of the model building
   code and will be fed from the downloaded data in the .run() loop, below.
+
   Args:
     batch_size: The batch size will be baked into both placeholders.
+
   Returns:
     images_placeholder: Images placeholder.
     labels_placeholder: Labels placeholder.
@@ -42,177 +58,163 @@ def placeholder_inputs(batch_size):
   # image and label tensors, except the first dimension is now batch_size
   # rather than the full size of the train or test data sets.
   images_placeholder = tf.placeholder(tf.float32, shape=(batch_size,
-                                                         model_build.FRAMES,
+                                                         16,
                                                          model_build.IMAGE_SIZE,
                                                          model_build.IMAGE_SIZE,
                                                          model_build.CHANNELS))
+  #images_placeholder = tf.placeholder(tf.float32, shape=(batch_size,28,28,1))
   labels_placeholder = tf.placeholder(tf.int64, shape=(batch_size))
   return images_placeholder, labels_placeholder
 
 
 def average_gradients(tower_grads):
-  """Calculate the average gradient for each shared variable across all towers.
-  Note that this function provides a synchronization point across all towers.
-  Args:
-    tower_grads: List of lists of (gradient, variable) tuples. The outer list
-      is over individual gradients. The inner list is over the gradient
-      calculation for each tower.
-  Returns:
-     List of pairs of (gradient, variable) where the gradient has been averaged
-     across all towers.
-  """
   average_grads = []
   for grad_and_vars in zip(*tower_grads):
-    # Note that each grad_and_vars looks like the following:
-    #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
     grads = []
     for g, _ in grad_and_vars:
-      # Add 0 dimension to the gradients to represent the tower.
       expanded_g = tf.expand_dims(g, 0)
-
-      # Append on a 'tower' dimension which we will average over below.
       grads.append(expanded_g)
-
-    # Average over the 'tower' dimension.
     grad = tf.concat(0, grads)
     grad = tf.reduce_mean(grad, 0)
-
-    # Keep in mind that the Variables are redundant because they are shared
-    # across towers. So .. we will just return the first tower's pointer to
-    # the Variable.
     v = grad_and_vars[0][1]
     grad_and_var = (grad, v)
     average_grads.append(grad_and_var)
   return average_grads
 
-def _variable_on_cpu(name, shape, initializer,cpu_id):
+def tower_loss(name_scope,logit,labels):
+  cross_entropy_mean = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logit, labels))
+  tf.scalar_summary(name_scope + 'cross entropy', cross_entropy_mean)
+  weight_decay_loss = tf.add_n(tf.get_collection('losses', name_scope))
+  tf.scalar_summary(name_scope + 'weight decay loss', weight_decay_loss)
+  tf.add_to_collection('losses',cross_entropy_mean)
+
+  losses = tf.get_collection('losses', name_scope)
+
+  # Calculate the total loss for the current tower.
+  total_loss = tf.add_n(losses, name='total_loss')
+  tf.scalar_summary(name_scope + 'total loss', total_loss)
+
+  # Compute the moving average of all individual losses and the total loss.
+  loss_averages = tf.train.ExponentialMovingAverage(0.99, name='loss')
+  loss_averages_op = loss_averages.apply(losses + [total_loss])
+  with tf.control_dependencies([loss_averages_op]):
+    total_loss = tf.identity(total_loss)
+  return total_loss
+
+def tower_acc(logit,labels):
+  correct_pred = tf.equal(tf.argmax(logit,1), labels)
+  accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+  return accuracy
+
+def _variable_on_cpu(name, shape, initializer):
   with tf.device('/cpu:0'):
     var = tf.get_variable(name, shape, initializer=initializer)
   return var
 
-def _variable_with_weight_decay(name, shape, stddev, wd,cpu_id):
-  var = _variable_on_cpu(name, shape,tf.truncated_normal_initializer(stddev=stddev),cpu_id)
+def _variable_with_weight_decay(name, shape, wd):
+  var = _variable_on_cpu(name, shape,tf.contrib.layers.xavier_initializer())
   if wd is not None:
     weight_decay = tf.mul(tf.nn.l2_loss(var), wd, name='weight_loss')
     tf.add_to_collection('losses', weight_decay)
   return var
 
-def tower_loss(scope,images,labels,weights,biases,batch_size):
-  """Calculate the total loss on a single tower running the CIFAR model.
-  Args:
-    scope: unique prefix string identifying the CIFAR tower, e.g. 'tower_0'
-  Returns:
-     Tensor of shape [] containing the total loss for a batch of data
-  """
-  logits = model_build.inference_c3d(images,0.5,batch_size,weights,biases) 
-  cross_entropy_mean = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels))
-  cross_entropy_mean = tf.Print(cross_entropy_mean,[cross_entropy_mean],'cross_entropy_mean: ')
-  tf.add_to_collection('losses', cross_entropy_mean)
-
-  losses = tf.get_collection('losses', scope)
-
-  # Calculate the total loss for the current tower.
-  total_loss = tf.add_n(losses, name='total_loss')
-  total_loss = tf.Print(total_loss,[total_loss],'loss: ')
-
-  # Compute the moving average of all individual losses and the total loss.
-  loss_averages = tf.train.ExponentialMovingAverage(0.9, name='loss')
-  loss_averages_op = loss_averages.apply(losses + [total_loss])
-
-  correct_pred = tf.equal(tf.argmax(logits,1), labels)
-  accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-  with tf.control_dependencies([loss_averages_op]):
-    total_loss = tf.identity(total_loss)
-  return total_loss,accuracy
-
 def run_training():
   # Get the sets of images and labels for training, validation, and
   # Tell TensorFlow that the model will be built into the default Graph.
   with tf.Graph().as_default():
+    global_step = tf.get_variable('global_step', [],initializer = tf.constant_initializer(0),trainable = False)
     images_placeholder, labels_placeholder = placeholder_inputs(FLAGS.batch_size*gpu_num)
-    lr = tf.placeholder(tf.float32, shape=[])
-    tower_grads = []
-    accuracys = []
-    opt = tf.train.AdamOptimizer(learning_rate=lr)
+    tower_grads1 = []
+    tower_grads2 = []
+    logits = []
+    opt1 = tf.train.AdamOptimizer(1e-4)
+    opt2 = tf.train.AdamOptimizer(2e-4)
     for gpu_index in range(0,gpu_num):
-      with tf.device('/gpu:%d' % (gpu_index)):
-        with tf.name_scope('%s_%d' % ('c3d', gpu_index)) as scope:
-          with tf.variable_scope('c3d_var') as var_scope:
+      with tf.device('/gpu:%d' % gpu_index):
+        with tf.name_scope('%s_%d' % ('houxin', gpu_index)) as scope:
+          with tf.variable_scope('var_name') as var_scope:
             weights = {
-              'wc1': _variable_with_weight_decay('wc1',[3, 3, 3, 3, 64],0.04,0.00,gpu_index),
-              'wc2': _variable_with_weight_decay('wc2',[3, 3, 3, 64, 128],0.04,0.00,gpu_index),
-              'wc3a': _variable_with_weight_decay('wc3a',[3, 3, 3, 128, 256],0.04,0.00,gpu_index),
-              'wc3b': _variable_with_weight_decay('wc3b',[3, 3, 3, 256, 256],0.04,0.00,gpu_index),
-              'wc4a': _variable_with_weight_decay('wc4a',[3, 3, 3, 256, 512],0.04,0.00,gpu_index),
-              'wc4b': _variable_with_weight_decay('wc4b',[3, 3, 3, 512, 512],0.04,0.00,gpu_index),
-              'wc5a': _variable_with_weight_decay('wc5a',[1, 3, 3, 512, 512],0.04,0.00,gpu_index),
-              'wc5b': _variable_with_weight_decay('wc5b',[1, 3, 3, 512, 512],0.04,0.00,gpu_index),
-              'wd1': _variable_with_weight_decay('wd1',[8192, 4096],0.04,0.005,gpu_index),
-              'wd2': _variable_with_weight_decay('wd2',[4096, 4096],0.04,0.005,gpu_index),
-              'out': _variable_with_weight_decay('wout',[4096, model_build.NUM_CLASSES],0.04,0.005,gpu_index)
-            }
+              'wc1': _variable_with_weight_decay('wc1',[3, 3, 3, 3, 64],0.0005),
+              'wc2': _variable_with_weight_decay('wc2',[3, 3, 3, 64, 128],0.0005),
+              'wc3a': _variable_with_weight_decay('wc3a',[3, 3, 3, 128, 256],0.0005),
+              'wc3b': _variable_with_weight_decay('wc3b',[3, 3, 3, 256, 256],0.0005),
+              'wc4a': _variable_with_weight_decay('wc4a',[3, 3, 3, 256, 512],0.0005),
+              'wc4b': _variable_with_weight_decay('wc4b',[3, 3, 3, 512, 512],0.0005),
+              'wc5a': _variable_with_weight_decay('wc5a',[3, 3, 3, 512, 512],0.0005),
+              'wc5b': _variable_with_weight_decay('wc5b',[3, 3, 3, 512, 512],0.0005),
+              'wd1': _variable_with_weight_decay('wd1',[8192, 4096],0.0005),
+              'wd2': _variable_with_weight_decay('wd2',[4096, 4096],0.0005),
+              'out': _variable_with_weight_decay('wout',[4096, model_build.NUM_CLASSES],0.0005)
+              }
             biases = {
-              'bc1': _variable_with_weight_decay('bc1',[64],0.04,0.0,gpu_index),
-              'bc2': _variable_with_weight_decay('bc2',[128],0.04,0.0,gpu_index),
-              'bc3a': _variable_with_weight_decay('bc3a',[256],0.04,0.0,gpu_index),
-              'bc3b': _variable_with_weight_decay('bc3b',[256],0.04,0.0,gpu_index),
-              'bc4a': _variable_with_weight_decay('bc4a',[512],0.04,0.0,gpu_index),
-              'bc4b': _variable_with_weight_decay('bc4b',[512],0.04,0.0,gpu_index),
-              'bc5a': _variable_with_weight_decay('bc5a',[512],0.04,0.0,gpu_index),
-              'bc5b': _variable_with_weight_decay('bc5b',[512],0.04,0.0,gpu_index),
-              'bd1': _variable_with_weight_decay('bd1',[4096],0.04,0.0,gpu_index),
-              'bd2': _variable_with_weight_decay('bd2',[4096],0.04,0.0,gpu_index),
-              'out': _variable_with_weight_decay('bout',[model_build.NUM_CLASSES],0.04,0.0,gpu_index),
-            }
+              'bc1': _variable_with_weight_decay('bc1',[64],0.000),
+              'bc2': _variable_with_weight_decay('bc2',[128],0.000),
+              'bc3a': _variable_with_weight_decay('bc3a',[256],0.000),
+              'bc3b': _variable_with_weight_decay('bc3b',[256],0.000),
+              'bc4a': _variable_with_weight_decay('bc4a',[512],0.000),
+              'bc4b': _variable_with_weight_decay('bc4b',[512],0.000),
+              'bc5a': _variable_with_weight_decay('bc5a',[512],0.000),
+              'bc5b': _variable_with_weight_decay('bc5b',[512],0.000),
+              'bd1': _variable_with_weight_decay('bd1',[4096],0.000),
+              'bd2': _variable_with_weight_decay('bd2',[4096],0.000),
+              'out': _variable_with_weight_decay('bout',[model_build.NUM_CLASSES],0.000),
+              }
+          varlist1 = weights.values()
+          varlist2 = biases.values()
+          logit = model_build.inference_c3d(images_placeholder[gpu_index*FLAGS.batch_size:(gpu_index+1)*FLAGS.batch_size,:,:,:,:],
+                  0.5,FLAGS.batch_size,weights,biases) 
+          loss = tower_loss(scope,logit,
+                  labels_placeholder[gpu_index*FLAGS.batch_size:(gpu_index+1)*FLAGS.batch_size]) 
+          grads1 = opt1.compute_gradients(loss,varlist1)
+          grads2 = opt2.compute_gradients(loss,varlist2)
+          tower_grads1.append(grads1)
+          tower_grads2.append(grads2)
+          logits.append(logit)
           tf.get_variable_scope().reuse_variables()
-          loss,accuracy = tower_loss(scope,images_placeholder[gpu_index*FLAGS.batch_size:(gpu_index+1)*FLAGS.batch_size,:,:,:,:],
-                  labels_placeholder[gpu_index*FLAGS.batch_size:(gpu_index+1)*FLAGS.batch_size],weights,biases,FLAGS.batch_size) 
-          grads = opt.compute_gradients(loss)
-          tower_grads.append(grads)
-          accuracys.append(accuracy) 
-    grads = average_gradients(tower_grads)
-    apply_gradient_op = opt.apply_gradients(grads)
+    logits = tf.concat(0,logits)
+    accuracy = tower_acc(logits,labels_placeholder)
+    tf.scalar_summary('accuracy', accuracy)
+    grads1 = average_gradients(tower_grads1)
+    grads2 = average_gradients(tower_grads2)
+    apply_gradient_op1 = opt1.apply_gradients(grads1)
+    apply_gradient_op2 = opt2.apply_gradients(grads2,global_step=global_step)
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY)
     variables_averages_op = variable_averages.apply(tf.trainable_variables())
-    train_op = tf.group(apply_gradient_op, variables_averages_op) 
-    init_op = tf.initialize_all_variables()
+    train_op = tf.group(apply_gradient_op1,apply_gradient_op2,variables_averages_op) 
+    null_op = tf.no_op()
     # Create a saver for writing training checkpoints.
-    saver = tf.train.Saver()
+    saver = tf.train.Saver(weights.values()+biases.values())
+    init = tf.initialize_all_variables()
     # Create a session for running Ops on the Graph.
     sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
-    # Run the Op to initialize the variables.
-    init = tf.initialize_all_variables()
     sess.run(init)
-    #saver.restore(sess,"./model_c3d_ucf101_0713_pm")
-    # And then after everything is built, start the training loop.
-    lr_step = 2000
+    saver.restore(sess,"sports1m_finetuning_ucf101.model")
+    #create summary writter
+    merged = tf.merge_all_summaries()
+    train_writer = tf.train.SummaryWriter('./visual_logs/train',sess.graph)
+    test_writer = tf.train.SummaryWriter('./visual_logs/test',sess.graph)
     for step in xrange(FLAGS.max_steps):
-      # Fill a feed dictionary with the actual set of images and labels
-      # for this particular training step.
       start_time = time.time()
-      print ('learning rate: ' + str(FLAGS.learning_rate*pow(0.1,(int)(step/lr_step))))
-      train_images,train_labels = input_data.ReadDataLabelFromFile_16('list/train_ucf101.trainVideos',FLAGS.batch_size*gpu_num)
+      train_images,train_labels = input_data.ReadDataLabelFromFile('list/train.list',FLAGS.batch_size*gpu_num)
       sess.run(train_op, feed_dict={images_placeholder: train_images, 
           labels_placeholder: train_labels,
-          lr: FLAGS.learning_rate*pow(0.1,(int)(step/lr_step))
           }
           )
       duration = time.time() - start_time
       print('Step %d: %.3f sec' % (step, duration))
-
       # Save a checkpoint and evaluate the model periodically.
-      if (step) % 50 == 0 or (step + 1) == FLAGS.max_steps:
-        saver.save(sess, 'models/c3d_ucf101_model', global_step=step)
+      if (step) % 10 == 0 or (step + 1) == FLAGS.max_steps:
+        saver.save(sess, 'models/c3d_ucf_model', global_step=step)
         print('Training Data Eval:')
-        acc = sess.run(accuracys, feed_dict={images_placeholder: train_images, labels_placeholder: train_labels})
-        acc = np.array(acc).mean()
+        summary,acc = sess.run([merged,accuracy], feed_dict={images_placeholder: train_images, labels_placeholder: train_labels})
         print ("accuracy: " + "{:.5f}".format(acc))
+        train_writer.add_summary(summary, step)
         print('Validation Data Eval:')
-        val_images,val_labels = input_data.ReadDataLabelFromFile_16('list/test_ucf101.trainVideos',FLAGS.batch_size*gpu_num)
-        acc = sess.run(accuracys, feed_dict={images_placeholder: val_images, labels_placeholder: val_labels})
-        acc = np.array(acc).mean()
+        val_images,val_labels = input_data.ReadDataLabelFromFile('list/test.list',FLAGS.batch_size*gpu_num)
+        summary,acc = sess.run([merged,accuracy], feed_dict={images_placeholder: val_images, labels_placeholder: val_labels})
         print ("accuracy: " + "{:.5f}".format(acc))
+        test_writer.add_summary(summary, step)
+  sv.stop()
   print("done")
 
 def main(_):
